c5577f89aebf06b21870f665bec9db15393104c2
FLance03 <60454430+FLance03@users.noreply.github.com>
2020-03-25 22:02:45 +0800

    Create route.php

---
+++ mini_ismis/route.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    $conn = new mysqli('localhost','root','');
+
+    if ($conn->connect_error){
+        die('Connection Failed: '.$conn->connect_error);
+    }
+
+    function AdjustMilitary($hour,$minute){
+        if ($hour>=1 && $hour<=6){
+            $hour += 12;
+        }else if ($hour==13){
+            $hour = 19;
+        }
+        return "{$hour}:{$minute}:00";
+    }
+
+    if (isset($_GET['tableName'])){
+        if (isset($_GET['insert']) && $_GET['insert']=='1'){
+            echo 'a';print_r($_POST['subject']==false);
+            echo 'b';print_r($_POST['student']==false);
+            echo 'c';print_r($_POST['schedule']==false);
+            if (isset($_POST['subject']) && isset($_POST['student']) && isset($_POST['schedule'])){
+                $sql = "INSERT INTO mini_ismis.subject_student(subject_id,student_id) VALUES
+                                                                ({$_POST['subject']},{$_POST['student']})";
+                if ($result=$conn->query($sql)==false){
+                    die($conn->error);
+                }
+                $sql = "INSERT INTO mini_ismis.classes(schedule_id,student_id) VALUES
+                                                        ({$_POST['schedule']},{$_POST['student']})";
+                if ($result=$conn->query($sql)==false){
+                    die($sql);
+                }
+                header("Location: ../users/students.php");
+                exit();
+            }
+            if (isset($_POST['faculty']) && isset($_POST['chosenSubject']) && isset($_POST['day_of_week']) && isset($_POST['startHour'])
+                && isset($_POST['startMinute']) && isset($_POST['endHour']) && isset($_POST['endMinute'])){
+                    $faculty = $_POST['faculty'];
+                    $subject = $_POST['chosenSubject'];
+                    $sql = "INSERT INTO mini_ismis.schedules(subject_id,faculty_id) VALUES({$subject},{$faculty})";
+                    if($conn->query($sql)==false){
+                        die($conn->error);
+                    }
+                    $insertId = $conn->insert_id;
+                    for ($i=0 ; $i<count($_POST['day_of_week']) ; $i++){
+                        $start = AdjustMilitary($_POST['startHour'][$i],$_POST['startMinute'][$i]);
+                        $end = AdjustMilitary($_POST['endHour'][$i],$_POST['endMinute'][$i]);
+                        $enumDayOfWeek = array('MON','TUE','WED','THU','FRI');
+                        $dayOfWeek = $enumDayOfWeek[$_POST['day_of_week'][$i]];
+
+                        $sql = "INSERT INTO mini_ismis.schedule_session(schedule_id,day_of_week,start,end) VALUES(
+                                                {$insertId},'{$dayOfWeek}','{$start}','{$end}')";
+                        if($conn->query($sql)==false){
+                            die($conn->error);
+                        }
+                    }
+                    $sql = "SELECT * FROM mini_ismis.subject_faculty WHERE subject_id={$subject} AND faculty_id={$faculty}";
+                    $record = $conn->query($sql);
+                    if($record==false){
+                        die($conn->error);
+                    }
+                    if ($record->num_rows==0){
+                        $sql = "INSERT INTO mini_ismis.subject_faculty(faculty_id,subject_id) VALUES({$faculty},{$subject})";
+                    }
+                    header("Location: ../schedule.php");
+                    exit();
+                }
+        }else {
+            if ($_GET['tableName']=='subject_student'){
+                header("Location: ./subject_student.php");
+                exit();
+            }else if($_GET['tableName']=='schedule' && isset($_GET['subject'])){
+                header("Location: ./schedule.php");
+                exit();
+            }
+        }
+    }
+?>
+<script>
+// javascript:history.go(-1)
+</script>
