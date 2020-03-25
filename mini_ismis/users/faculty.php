7bddf70642746004e4b571a1083b92df5bb66ea3
X500658 <30663695+X500658@users.noreply.github.com>
2020-03-26 00:27:01 +0800

    locked

--- mini_ismis/users/faculty.php
+++ mini_ismis/users/faculty.php
@@ -85,7 +85,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete') {
 <head>
     <title>minIsmis - Faculty</title>
 </head>
-<<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" type="text/css" href="../style.css" />
